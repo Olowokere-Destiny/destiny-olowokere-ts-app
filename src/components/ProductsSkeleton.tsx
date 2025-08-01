5cc295097891da638191c67d571c25a5ddaf1691
Olowokere_Destiny <destinyolowokere@gmail.com>
2025-08-01 21:38:12 +0100

    skeleton improvement

--- src/components/ProductsSkeleton.tsx
+++ src/components/ProductsSkeleton.tsx
@@ -1,10 +1,23 @@
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
 const ProductsSkeleton = () => {
+  const { theme } = useContext(ThemeContext)!;
+  let containerClasses = "";
+  if (theme === "light" || theme === "dark") {
+    containerClasses = "flex flex-col";
+  } else if (theme === "forest") {
+    containerClasses = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3";
+  }
+
   return (
-    <div className="flex flex-col gap-4 mt-8">
+    <div className={`gap-4 mt-8 ${containerClasses}`}>
       {Array.from({ length: 8 }).map((_, index) => (
         <div
           key={index}
-          className="animate-pulse bg-[var(--skeleton-bg)] h-18 rounded-md"
+          className={`animate-pulse bg-[var(--skeleton-bg)] ${
+            theme === "forest" ? "h-44 rounded-2xl" : "h-18 rounded-md"
+          }`}
         />
       ))}
     </div>
--- src/components/ThemeContext.tsx
+++ src/components/ThemeContext.tsx
@@ -1,8 +1,10 @@
 import { createContext } from "react";
 import type { Theme } from "../lib/types";
 
+// "theme" is the current theme value, and "toggleTheme" is a function that changes the theme
 interface Props {
   theme: Theme;
   toggleTheme: (theme: Theme) => void;
 }
+
 export const ThemeContext = createContext<Props | null>(null);

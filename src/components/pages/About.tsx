092e7955630135f0e84510d1fcae1fe6d6d3b164
Olowokere_Destiny <destinyolowokere@gmail.com>
2025-08-02 18:32:24 +0100

    changed favicon, title. Text fix on mobile

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -4,17 +4,26 @@ import { ThemeContext } from "../ThemeContext";
 const About = () => {
   const { theme } = useContext(ThemeContext)!;
   return (
-    <div className='w-full min-h-[calc(100vh-30px)]'>
-        <div className={`space-y-4 mt-6 md:mt-0 ${theme === "forest" && "w-max mx-auto"}`}>
-        <h1 className={`text-(length:--title-size) font-(--title-weight) text-[var(--title-color)] ${theme === "forest" && "text-center"}`}>
+    <div className="w-full min-h-[calc(100vh-30px)]">
+      <div
+        className={`space-y-4 mt-6 md:mt-0 ${
+          theme === "forest" && "mx-auto md:w-max"
+        }`}
+      >
+        <h1
+          className={`text-(length:--title-size) font-(--title-weight) text-[var(--title-color)] ${
+            theme === "forest" && "text-center"
+          }`}
+        >
           About Us
         </h1>
         <p className="text-[var(--text)] text-(length:--subtitle-size) font-(--subtitle-weight)">
-          We are a team of passionate engineers and designers. - Destiny Olowokere.
+          We are a team of passionate engineers and designers. - Destiny
+          Olowokere.
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
